bfa0854cdeb25b367cb84ec2d395bd94c86fc4cf
Peter Gill <peter@majorsilence.com>
2026-02-19 17:12:08 -0330

    feat: add Majorsilence.Reporting.UI project with initial Avalonia application structure

---
+++ Majorsilence.Reporting.UI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Majorsilence.Reporting.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
